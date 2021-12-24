ba2fd27e6943df14ef62465fb0639d38c4dab907
Saad allah Samadi <ssamadi@e1r11p6.1337.ma>
2021-12-24 16:01:37 +0100

    add new logic

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,21 +1,44 @@
 #include <vector>
-// #include "vector.hpp"
+#include "vector.hpp"
 #include <iostream>
+
+
+
 
 int main()
 {
     // try
     // {
-    //    std::vector<int>  tt(-11);
+        std::vector<int> tt;
+        tt.push_back(1);
+        tt.push_back(2);
+        tt.push_back(3);
+        tt.push_back(4);
+
+    // tt.push_back(55);
+    //    std::vector<int>::iterator it = tt.begin();
+    //    std::vector<int>::iterator it2(it);
+
+    //    it2 =  tt.end()  - 1;
+    //    std::cout << *it << std::endl;
+
+    // int k = 5;
+    // int *tt = &k;
+
+    // std::cout << ++(*tt) << std::endl;
+
+    std::vector<int>::iterator it1 = tt.begin();
+    // *it1;
+    std::cout << *it1 << std::endl;
+    
+    
+    ft::vector<int>::iterator my;
+    std::cout << *my << std::endl;
     // }
     // catch(const std::exception& e)
     // {
     //     std::cerr << e.what() << '\n';
     // }
     // ft::vector<int> vv(5, 6);
-    std::vector<int>  tt(11);
-    std::vector<int>::iterator it;
-    constexpr a;
-    it.base();
     // std::cout << *it << std::endl;
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -6,24 +6,76 @@
 
 namespace ft
 {
-	template < class _Tp, class _Allocator = std::allocator<_Tp> >
-	class vector;
-}
-
-template < class _Tp, class _Allocator>
-class ft::vector
+template < class category, class T, class Distanse = ptrdiff_t, class Pointer =  T*, class Reference = T&>
+struct itrator
 {
+	typedef		T 			value_type;
+	typedef		Distanse 	difference_pointer;
+	typedef		Pointer		pointer;
+	typedef		Reference 	reference;
+	typedef		category	itrator_categor;
+};
+template <class T>
+class random_access_iterator: public itrator< std::random_access_iterator_tag, T>
+{
+	private:
+		T *it;
+		typedef random_access_iterator my_iterator;
+	public:
+		//constaractors and destractor
+		random_access_iterator(){}
+		random_access_iterator(T *value): it(value){}
+		random_access_iterator(const random_access_iterator& cp): it(cp.it){}
+		~random_access_iterator(){}
+
+		// start overload obertator
+		void operator=(my_iterator &cp)
+		{
+			//possible deep copy, 
+			this->it = cp.it;
+		}
+		bool operator==(my_iterator &cp)
+		{
+			return (this->it == cp.it);
+		}
+		bool operator!=(my_iterator &cp)
+		{
+			return (this->it != cp.it);
+		}
+
+		//prefix oprator ++ mean ++a;
+		my_iterator &operator++()
+		{
+			++it;
+			return(*this);
+		}
+		my_iterator operator++(int)
+		{
+			my_iterator __tmp(*this); 
+			++(*this); 
+			return __tmp;
+		}
+		T& operator*()
+		{
+			return *it;
+		}
+	
+};
+template < class T, class _Allocator = std::allocator<T> >
+class vector 
+{
+
 private:
-	_Tp *dy_arr;
+	T* dy_arr;
 public:
 	typedef size_t                                      size_type;
-	typedef _Tp                                         value_type;
+	typedef T                                         	value_type;
 	typedef _Allocator                                  allocator_type;
 	typedef typename allocator_type::reference	        reference;
 	typedef typename allocator_type::const_reference    const_reference;
 	typedef typename allocator_type::pointer            pointer;
 	typedef typename allocator_type::const_pointer      const_pointer;
-	typedef __wrap_iter<pointer>						irerator;
+	typedef ft::random_access_iterator<value_type>			iterator;
 
 	vector()
 	{
@@ -31,10 +83,7 @@ public:
 	}
 	vector(size_t element, int  contant = 0)
 	{
-		_Allocator a;
-		dy_arr = a.allocate(element);
-		for (int i = 0; i < element; i++)
-			dy_arr[i] = contant;
+		
 	}
 	~vector()
 	{
@@ -43,57 +92,15 @@ public:
 };
 
 
-template<class ITER>
-class __wrap_iter
-{
-private:
-	ITER it_P;
-
-public:
-	// template <class _Iter1, class _Iter2>
-	// friend bool operator==(const __wrap_iter<_Iter1>&, const __wrap_iter<_Iter2>&)
-	// {
-
-	// }
-
-	// template <class _Iter1, class _Iter2>
-	// friend bool operator<(const __wrap_iter<_Iter1>&, const __wrap_iter<_Iter2>&)
-	// {
-
-	// }
-
-	// template <class _Iter1, class _Iter2>
-	// friend bool operator!=(const __wrap_iter<_Iter1>&, const __wrap_iter<_Iter2>&)
-	// {
-
-	// }
-
-	// template <class _Iter1, class _Iter2>
-	// friend bool operator>(const __wrap_iter<_Iter1>&, const __wrap_iter<_Iter2>&)
-	// {
-
-	// }
-
-	// template <class _Iter1, class _Iter2>
-	// friend bool operator>=(const __wrap_iter<_Iter1>&, const __wrap_iter<_Iter2>&)
-	// {
-
-	// }
-
-	// template <class _Iter1, class _Iter2>
-	// friend bool operator<=(const __wrap_iter<_Iter1>&, const __wrap_iter<_Iter2>&)
-	// {
-
-	// }
-
-	// template <class _Iter1>
-	// friend __wrap_iter<_Iter1> operator+(__wrap_iter<_Iter1> , __wrap_iter<_Iter1>)
-	// {
-
-	// }
 
 
-	
+
+
+
+
+
+
 }
+
 
 #endif
