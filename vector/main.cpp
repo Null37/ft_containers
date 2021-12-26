2a879ea9deb43188d2936d12ace65f7c45455ff6
Null <null@localhost.localdomain>
2021-12-26 16:34:19 +0100

    zamel

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,19 +1,24 @@
 #include <vector>
-#include "vector.hpp"
+ #include "vector.hpp"
 #include <iostream>
 
+#include <iterator>
 
 
+struct lol
+{
+    
+};
 
 int main()
 {
     // try
     // {
         std::vector<int> tt;
-        tt.push_back(1);
-        tt.push_back(2);
-        tt.push_back(3);
-        tt.push_back(4);
+        // tt.push_back(1);
+        // tt.push_back(2);
+        // tt.push_back(3);
+        // tt.push_back(4);
 
     // tt.push_back(55);
     //    std::vector<int>::iterator it = tt.begin();
@@ -27,11 +32,11 @@ int main()
 
     // std::cout << ++(*tt) << std::endl;
 
-    std::vector<int>::iterator it1(tt.begin());
-    // *it1 = 5;
-    // std::cout << *it1 << std::endl;
-    ft::vector<int>::iterator my(tt.begin());
-    *my = 5;
+    // std::vector<int>::iterator it1(tt.begin());
+    // // *it1 = 5;
+    // std::cout << *it3 << std::endl;
+    ft::vector<int>::iterator my;
+    // *my = 5;
     std::cout << *my << std::endl;
     // }
     // catch(const std::exception& e)
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -1,32 +1,30 @@
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
+#include "../iterator_traits.hpp"
 
 #include <iostream>
 
+
 namespace ft
 {
-template < class category, class T, class Distanse = ptrdiff_t, class Pointer =  T*, class Reference = T&>
-struct itrator
-{
-	typedef		T 			value_type;
-	typedef		Distanse 	difference_pointer;
-	typedef		Pointer		pointer;
-	typedef		Reference 	reference;
-	typedef		category	itrator_categor;
-};
+
 template <class T>
-class random_access_iterator: public itrator< std::random_access_iterator_tag, T>
+class my_iterator 
 {
 	private:
-		T *it;
-		typedef random_access_iterator my_iterator;
+		T it;
 	public:
+		typedef typename ft::iterator_traits<T>::value_type			value_type;
+		typedef typename ft::iterator_traits<T>::pointer			pointer;
+		typedef typename ft::iterator_traits<T>::reference			reference;
+		typedef typename ft::iterator_traits<T>::difference_type	difference_type;
+		typedef typename ft::iterator_traits<T>::iterator_category 	iterator_category;
 		//constaractors and destractor
-		random_access_iterator(){}
-		random_access_iterator(T *value): it(value){}
-		//random_access_iterator(const random_access_iterator& cp): it(cp.it){}
-		~random_access_iterator(){}
+		my_iterator(){it = NULL;}
+		my_iterator(T *value): it(value){}
+		//my_iterator(const my_iterator& cp): it(cp.it){}
+		~my_iterator(){}
 
 		// start overload obertator
 		void operator=(my_iterator &cp)
@@ -43,48 +41,71 @@ class random_access_iterator: public itrator< std::random_access_iterator_tag, T
 			return (this->it != cp.it);
 		}
 
+		reference operator*()
+		{
+			return *it;
+		}
+
+		pointer operator->()
+		{
+			return *it;
+		}
+
 		//prefix oprator ++ mean ++a;
 		my_iterator &operator++()
 		{
 			++it;
 			return(*this);
 		}
+
 		my_iterator operator++(int)
 		{
 			my_iterator __tmp(*this); 
 			++(*this); 
 			return __tmp;
 		}
-		T& operator*()
+		my_iterator &operator--()
 		{
-			return *it;
-		}
-		T* operator->()
-		{
-			return *it;
+			--it;
+			return(*this);
 		}
 
-	
+		my_iterator operator--(int)
+		{
+			my_iterator __tmp(*this); 
+			--(*this); 
+			return __tmp;
+		}
+		
+
+		
+
+		
+
 };
+
 template < class T, class _Allocator = std::allocator<T> >
 class vector 
 {
 
-private:
-	T* dy_arr;
 public:
 	typedef size_t                                      size_type;
 	typedef T                                         	value_type;
 	typedef _Allocator                                  allocator_type;
 	typedef typename allocator_type::reference	        reference;
 	typedef typename allocator_type::const_reference    const_reference;
-	typedef typename allocator_type::pointer            pointer;
-	typedef typename allocator_type::const_pointer      const_pointer;
-	typedef ft::random_access_iterator<value_type>			iterator;
+	typedef typename allocator_type::pointer           	pointer; // T*
+	typedef typename allocator_type::const_pointer      const_pointer; //const T*
+	typedef ft::my_iterator<pointer>					iterator;
+	typedef ft::my_iterator<const pointer>				const_iterator;
 
+private:
+	pointer dy_arr;
+
+public:
 	vector()
 	{
-
+			
 	}
 	vector(size_t element, int  contant = 0)
 	{
@@ -94,6 +115,7 @@ public:
 	{
 		
 	}
+	
 };
 
 
